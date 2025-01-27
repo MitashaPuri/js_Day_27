b21e962227839b073818b01ec8c6747ac6fb00f3
MitashaPuri <99736676+MitashaPuri@users.noreply.github.com>
2025-01-27 10:41:09 +0530

    Add files via upload

---
+++ 27_jan.js
@@ -0,0 +1,104 @@
+//1.output 
+// Writing into an HTML element, using innerHTML.
+// Writing into the HTML output using document.write().
+// Writing into an alert box, using window.alert().
+// Writing into the browser console, using console.log() 
+
+// The document.write() method should only be used for testing.
+
+// 2. JavaScript Statements
+// JavaScript statements are composed of:
+// Values, Operators, Expressions, Keywords, and Comments
+
+
+// 3.JavaScript Values
+// The JavaScript syntax defines two types of values:
+// Fixed values=============Literals.
+// Variable values===========Variables.
+
+
+// 4.JavaScript Comments
+// Not all JavaScript statements are "executed".
+// Code after double slashes // or between /* and */ is treated as a comment.
+
+// 5. JavaScript Identifiers / Names
+// Identifiers are JavaScript names.
+// Identifiers are used to name variables and keywords, and functions.
+// The rules for legal names are the same in most programming languages.
+// A JavaScript name must begin with:
+// A letter (A-Z or a-z)
+// A dollar sign ($)
+// Or an underscore (_)
+
+// JavaScript is Case Sensitive
+//All JavaScript identifiers are case sensitive. 
+
+// 6.Let
+//Variables declared with let have Block Scope
+
+// Variables declared with let must be Declared before use
+
+// Variables declared with let cannot be Redeclared in the same scope
+
+// Before ES6 (2015), JavaScript did not have Block Scope.
+
+// JavaScript had Global Scope and Function Scope.
+
+// ES6 introduced the two new JavaScript keywords: let and const.
+
+// These two keywords provided Block Scope in JavaScript:
+
+
+// let and const have block scope.
+
+// let and const can not be redeclared.
+
+// let and const must be declared before use.
+
+// let and const does not bind to this.
+
+// let and const are not hoisted.
+
+
+// 7. const
+// The const keyword was introduced in ES6 (2015)
+
+// Variables defined with const cannot be Redeclared
+
+// Variables defined with const cannot be Reassigned
+
+// Variables defined with const have Block Scope
+
+
+// 8.operators
+// Types of JavaScript Operators
+// There are different types of JavaScript operators:
+
+// Arithmetic Operators
+// Assignment Operators
+// Comparison Operators
+// String Operators
+// Logical Operators
+// Bitwise Operators
+// Ternary Operators=======condition ? expressionIfTrue : expressionIfFalse;
+// Type Operators============typeof	Returns the type of a variable
+                            // instanceof	Returns true if an object is an instance of an object type
+
+
+
+//9.data types
+// JavaScript has 8 Datatypes
+// String
+// Number
+// Bigint
+// Boolean
+// Undefined
+// Null
+// Symbol
+// Object====>Built-in object types can be:
+// objects, arrays, dates, maps, sets, intarrays, floatarrays, promises, and more.
+
+
+// 10.Functions
+// A JavaScript function is a block of code designed to perform a particular task.
+// A JavaScript function is executed when "something" invokes it (calls it).
